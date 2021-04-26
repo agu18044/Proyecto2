3ae6165b7725b82a1ec2a8309b94777e2c2aa7c0
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-04-26 00:04:10 -0600

    creo archivo para la musica

---
+++ codigo_musica/codigo_musica.ino
@@ -0,0 +1,15 @@
+#include <SD.h>  
+#include <SPI.h>
+#include <TMRpcm.h>  
+#include <Wire.h>
+#define pinSD 10
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
